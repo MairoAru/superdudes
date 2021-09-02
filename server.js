058d0a5a3d8af60c3f672b768c305504937a03a3
hackerman <mairo.aru@gmail.com>
2021-09-02 17:48:38 +0300

    Supervillans

---
+++ server.js
@@ -0,0 +1,7 @@
+var superheroes = require('superheroes');
+var mySuperHeroName = superheroes.random();
+console.log(mySuperHeroName)
+
+var supervillans = require('supervillains');
+var mySuperWillanName = supervillans.all;
+console.log(mySuperWillanName)
